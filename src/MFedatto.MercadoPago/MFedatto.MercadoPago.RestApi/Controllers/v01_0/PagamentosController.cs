3aaf6308f9c2eb4d64bb031e9147b1477bd39ee7
Maurício Fedatto <mfedatto@gmail.com>
2019-07-31 22:06:13 -0300

    Skeleton

---
+++ src/MFedatto.MercadoPago/MFedatto.MercadoPago.RestApi/Controllers/v01_0/PagamentosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MFedatto.MercadoPago.RestApi.Controllers.v01_0
+{
+    [Controller]
+    [Route("/v01_0/pagamentos")]
+    public class PagamentosController : Controller
+    {
+        [HttpPost("pagamentos")]
+        public async Task<IActionResult> ReceberPagamento()
+        {
+            return View();
+        }
+    }
+}
